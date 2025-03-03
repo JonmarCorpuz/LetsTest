8b2197d64825a607dcaf8185c4afc9acab35702c
Jon <121370426+JonmarCorpuz@users.noreply.github.com>
2025-03-03 00:52:38 -0500

    Update providers.tf

--- DeployGKE/TerraformFiles/providers.tf
+++ DeployGKE/TerraformFiles/providers.tf
@@ -1,11 +1,15 @@
 terraform {
-
     required_version = ">= 1.0.0"
 
     required_providers {
-
+        google = {
+          source  = "hashicorp/google"
+        }
+    
+        google-beta = {
+          source  = "hashicorp/google-beta"
+        }
     }
-
 }
 
 provider "google" {
